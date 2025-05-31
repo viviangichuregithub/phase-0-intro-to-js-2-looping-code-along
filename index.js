e012115dc3c7f2f08ac5c534f2904e7b0f4589b1
Vivian Gichure <viviangichure@gmail.com>
2025-05-31 14:47:19 +0300

    count down

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Function to write thank you cards
+function writeCards(names, eventName) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages;
+}
+
+// Function to count down to zero
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
